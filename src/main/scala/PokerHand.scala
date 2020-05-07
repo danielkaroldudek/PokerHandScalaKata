890a0d640cd1f4ac7ba67c2ada7a20557b6945e0
Daniel Dudek <daniel.karol.dudek@gmail.com>
2020-05-07 21:42:01 +0200

    Refactoring code to clear the implementation.

--- src/main/scala/PokerHand.scala
+++ src/main/scala/PokerHand.scala
@@ -1,26 +1,35 @@
 object PokerHand {
-  def evaluate(hand: String): String = {
-    val figures: Map[Char, Int] = List('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A').zipWithIndex.toMap
-    case class Card(figure: Char, power: Int)
+  val figures: Map[Char, Int] = List('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A').zipWithIndex.toMap
+  case class Card(figure: Char, power: Int)
+  object Card {
+    def parse(input: String): Card = input.toList match {
+      case List(figure, _) => Card(figure, figures(figure))
+    }
+  }
 
-    val value: List[(Int, Int, Char)] = hand
+  case class CardOccurrence(occurrence: Int, cardPower: Int, figure: Char)
+  object CardOccurrence {
+    val create: ((Char, Array[Card])) => CardOccurrence = {
+      case (figure, cards) => CardOccurrence(cards.length, cards.head.power, figure)
+    }
+  }
+
+  implicit val cardOccurrenceOrdering: Ordering[CardOccurrence] =
+    Ordering.by[CardOccurrence, (Int, Int)](co => (co.occurrence, co.cardPower)).reverse
+
+  def evaluate(hand: String): String = {
+    val cardOccurrences: List[CardOccurrence] = hand
       .split(" ")
-      .map { card =>
-        card.toList match {
-          case List(figure, _) => Card(figure, figures(figure))
-        }
-      }
+      .map(Card.parse)
       .groupBy(_.figure)
-      .map {
-        case (figure, cards) => (cards.length, cards.head.power, figure)
-      }
+      .map(CardOccurrence.create)
       .toList
-      .sorted(Ordering[(Int, Int, Char)].reverse)
-    val (occurrences, _, figure) = value.head
-    if (occurrences == 2) {
-      "pair of : " + figure
+      .sorted
+    val maxCardOccurrence = cardOccurrences.head
+    if (maxCardOccurrence.occurrence == 2) {
+      "pair of : " + maxCardOccurrence.figure
     } else {
-      "high card : " + figure
+      "high card : " + maxCardOccurrence.figure
     }
   }
 }
