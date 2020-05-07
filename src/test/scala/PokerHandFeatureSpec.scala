1d3a2a98eebde09e256d4a610b2f35fe6e386800
Daniel Dudek <daniel.karol.dudek@gmail.com>
2020-05-07 23:40:04 +0200

    Adding matchers to the examples of other test notation.

--- src/test/scala/PokerHandFeatureSpec.scala
+++ src/test/scala/PokerHandFeatureSpec.scala
@@ -1,7 +1,8 @@
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
 
-class PokerHandFeatureSpec extends AnyFeatureSpec with GivenWhenThen {
+class PokerHandFeatureSpec extends AnyFeatureSpec with GivenWhenThen with Matchers {
   info("As Poker Player")
   info("...")
 
@@ -9,13 +10,13 @@ class PokerHandFeatureSpec extends AnyFeatureSpec with GivenWhenThen {
     Scenario("Poker hand should pick the highest card 7") {
 
       Given("I have 5 cards")
-      val cards = "5H 4C 6S 7S 2D"
+        val cards = "5H 4C 6S 7S 2D"
 
       When("I evaluate the hand")
-      val result = PokerHand.evaluate(cards)
+        val result = PokerHand.evaluate(cards)
 
       Then("I should have high card : 7")
-      assert(result == "high card : 7")
+      "high card : 7" == result
     }
   }
 }
--- src/test/scala/PokerHandFlatSpec.scala
+++ src/test/scala/PokerHandFlatSpec.scala
@@ -1,7 +1,8 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class PokerHandFlatSpec extends AnyFlatSpec {
+class PokerHandFlatSpec extends AnyFlatSpec with Matchers {
     "Poker hand" should "pick the highest card 7" in {
-      assert(PokerHand.evaluate("5H 4C 6S 7S 2D") == "high card : 7")
+      PokerHand.evaluate("5H 4C 6S 7S 2D") == "high card : 7"
     }
 }
